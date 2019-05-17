45dfee12e61a05ad98914299b7415cf9ebb30540
Beloule <trezaaqq@hotmail.fr>
2019-05-17 11:32:32 +0200

    Premier essai à canvas

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+var c = document.getElementById('monCanvas');
+var ctx = c.getContext("2d");
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 1500, 700);
+
+ctx.fillStyle = 'grey';
+ctx.fillRect(600, 300, 300, 400);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(650, 350, 70, 70);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(780, 350, 70, 70);
+
+ctx.fillStyle = 'red';
+ctx.fillRect(740, 480, 20, 20);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(650, 550, 200, 80);
+
+ctx.fillStyle = 'pink';
+ctx.fillRect(730, 600, 50, 80);
+ctx.rect(0, 0, 1500, 700);
